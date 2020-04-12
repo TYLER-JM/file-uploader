67f6b8bbbd204eaed31b7b2855c0e3dba847838f
tyler martin <t.mar92@gmail.com>
2020-04-12 12:38:26 -0300

    adds method to change the innerHTML of the label to display the file's name

--- src/components/FileUploadItem.vue
+++ src/components/FileUploadItem.vue
@@ -3,8 +3,8 @@
     <!-- <input type="file" name="1" id="file1"> -->
     <div class="input-group mb-3">
       <div class="custom-file mr-4">
-        <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+        <input type="file" class="custom-file-input" @change="updateMessage($event)" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+        <label class="custom-file-label" for="inputGroupFile01">{{ message }}</label>
       </div>
       <div v-if="isRemovable">
         <button class="bttn-unite bttn-danger" @click="$emit('removeFile', uniqueId)">X</button>
@@ -18,6 +18,16 @@ export default {
     isRemovable: Boolean,
     removeFile: Function,
     uniqueId: String
+  },
+  data() {
+    return {
+      message: 'Choose File'
+    }
+  },
+  methods: {
+    updateMessage(e) {
+      this.message = e.originalTarget.files[0].name
+    }
   }
 }
 </script>
